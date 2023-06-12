26e2000effb2d8fafa7e142e5f236d44023f0bfc
VycFl <v123ic@gmail.com>
2023-06-12 14:54:30 -0700

    fixed sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './shared/layout/header/header.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [HeaderComponent, FooterComponent, RouterOutlet, MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule, MatSidenavModule]
+  imports: [ FooterComponent, RouterOutlet, CommonModule, FlexLayoutModule, MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule, MatSidenavModule]
 })
 export class AppComponent {
   title = 'angular-portfolio';
